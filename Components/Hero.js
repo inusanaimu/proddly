7219eefe06b6d105a791a5e8fa1b68bd62b9e4d1
inusa naimu <inusanaimu@gmail.com>
2022-07-03 00:26:20 +0100

    Nav and Hero Responsive

--- Components/Hero.js
+++ Components/Hero.js
@@ -1,11 +1,42 @@
 import Image from 'next/dist/client/image'
 
 import lgBanner from '../public/lgBanner.svg'
+import smBanner from '../public/smBanner.svg'
+
+const Banner = () => {
+  return (
+    <>
+      <div className='hidden md:flex  '>
+        <Image
+          // layout='fill'
+          src={'/lgBanner.svg'}
+          alt='Banner'
+          height={867}
+          width={867}
+          className=' w-full'
+        />
+      </div>
+      <div className=' md:hidden'>
+        <Image
+          // layout='fill'
+          src={'/smBanner.svg'}
+          alt='Banner'
+          height={353}
+          width={385}
+          className=' w-full'
+        />
+      </div>
+    </>
+  )
+}
 
 function Hero() {
   return (
-    <div className=' w-full gap-0 grid grid-cols-3 content-center' style={{}}>
-      <div className='pl-28 my-auto'>
+    <div
+      className=' w-full gap-0 grid grid-flow-cols-1 md:grid-cols-3 content-center'
+      style={{}}
+    >
+      <div className='md:pl-28 md:my-auto'>
         <h1 className='mt-4 text-4xl '>Boost sales on Proddly</h1>
         <h5>
           Get your inventory infront of millions of shoppers and increase your
@@ -20,14 +51,7 @@ function Hero() {
       </div>
       <div className=' col-span-2 '>
         {' '}
-        <Image
-          // layout='fill'
-          src={'/lgBanner.svg'}
-          alt='Banner'
-          height={867}
-          width={867}
-          className=' w-full'
-        />
+        <Banner />
       </div>
     </div>
   )
--- Components/Nav.js
+++ Components/Nav.js
@@ -1,33 +1,56 @@
 import Image from 'next/dist/client/image'
 
 import Logo from '../public/logo.svg'
+import NavBtn from '../public/NavBtn.svg'
+
+const LogoComp = () => {
+  return (
+    <>
+      <div className='hidden md:flex'>
+        <Image src={'/Logo.svg'} alt='logo' height={53} width={173} />
+      </div>
+      <div className='flex md:hidden'>
+        <Image src={'/Logo.svg'} alt='logo' height={23} width={76} />
+      </div>
+    </>
+  )
+}
+
+const NavLinks = () => {
+  return (
+    <>
+      <ul className='flex-row text-sm hidden md:flex'>
+        <li className='mr-12'>
+          <a href='#'>Why join Proddly</a>
+        </li>
+        <li className='mr-12'>
+          <a href='#'>Features</a>
+        </li>
+        <li className='mr-12'>
+          <a href='#'>Testmonial</a>
+        </li>
+        <li className='mr-12'>
+          <a href='#'>How to join</a>
+        </li>
+        <li className='mr-12'>
+          <a href='#'>Pricing</a>
+        </li>
+      </ul>
+      <div className='md:hidden'>
+        <Image src={'/NavBtn.svg'} alt='logo' height={30} width={30} />
+      </div>
+    </>
+  )
+}
 
 function Nav() {
   return (
-    <nav className=' w-full z-10 absolute top-0'>
-      <div className='grid grid-cols-2 content-center my-8 mx-28'>
+    <nav className=' w-full md:z-10 md:absolute top-0'>
+      <div className='grid grid-cols-2 content-center my-2 mx-4 md:my-8 md:mx-28'>
         {' '}
-        <div className=''>
-          <Image src={'/Logo.svg'} alt='logo' height={53} width={173} />
-        </div>
+        <LogoComp />
         <div>
-          <ul className=' flex flex-row text-sm'>
-            <li className='mr-12'>
-              <a href='#'>Why join Proddly</a>
-            </li>
-            <li className='mr-12'>
-              <a href='#'>Features</a>
-            </li>
-            <li className='mr-12'>
-              <a href='#'>Testmonial</a>
-            </li>
-            <li className='mr-12'>
-              <a href='#'>How to join</a>
-            </li>
-            <li className='mr-12'>
-              <a href='#'>Pricing</a>
-            </li>
-          </ul>
+          <NavLinks />
         </div>
       </div>
     </nav>

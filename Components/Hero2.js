b4face38d742b6a626981caeb722098943beacaf
inusa naimu <inusanaimu@gmail.com>
2022-07-03 08:55:53 +0100

    Hero2 fully Responsive

--- Components/Hero2.js
+++ Components/Hero2.js
@@ -2,10 +2,10 @@ import Image from 'next/dist/client/image'
 
 import HeroBanner from '../public/hero2Banner.svg'
 
-function Hero2() {
+const ImageComp = () => {
   return (
-    <div className='grid grid-cols-2 gap-4 w-full px-24 content-center '>
-      <div className=' col-span-1'>
+    <>
+      <div className=' w-full md:hidden flex order-last md:order-none'>
         {' '}
         <Image
           src={'/Hero2Banner.svg'}
@@ -15,18 +15,39 @@ function Hero2() {
           className=''
         />
       </div>
-      <div className=' my-auto'>
-        <div>
-          <h3>Why Join Proddly</h3>
-          <ul className='list-disc'>
-            <li>Let shoppers find you easily and quickly</li>
+      <div className=' w-full hidden md:block order-last md:order-none'>
+        {' '}
+        <Image
+          src={'/Hero2Banner.svg'}
+          alt='Banner'
+          height={330}
+          width={318}
+          className=''
+        />
+      </div>
+    </>
+  )
+}
+
+function Hero2() {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 gap-4 w-full md:px-24 p-4 content-center '>
+      <ImageComp />
+      <div className=' my-auto leading-10'>
+        <div className='  '>
+          <h3 className=' text-xl text-bold mb-6'>Why Join Proddly</h3>
+          <ul className='list-disc list-inside mb-6'>
+            <li>
+              Let shoppers find you easily{' '}
+              <span className=' hidden md:inline-block '> and quickly.</span>
+            </li>
             <li>Increase your walk-in and online sales</li>
             <li>Get online without an e-commerce website</li>
           </ul>
         </div>
         <div>
-          <h3>Features</h3>
-          <ul className='list-disc'>
+          <h3 className='text-xl text-bold mb-6 '>Features</h3>
+          <ul className='list-disc list-inside  '>
             <li>Proddy 360 Dashboard</li>
             <li>View What`s in Store </li>
           </ul>

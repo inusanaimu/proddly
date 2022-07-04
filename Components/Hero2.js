3b41da6ac7d67c044a893fdf48dc2ae2f0d4e6f7
inusa naimu <inusanaimu@gmail.com>
2022-07-04 13:04:52 +0100

    Mini Fixes

--- Components/Hero2.js
+++ Components/Hero2.js
@@ -5,7 +5,7 @@ import Hero2Banner from '../public/hero2Banner.svg'
 const ImageComponent = () => {
   return (
     <>
-      <div className=' w-full md:hidden flex order-last md:order-none'>
+      <div className=' w-full block order-last md:order-none'>
         {' '}
         <Image
           src={'/Hero2Banner.svg'}
@@ -14,15 +14,15 @@ const ImageComponent = () => {
           width={867}
         />
       </div>
-      <div className=' w-full hidden md:block order-last md:order-none'>
+      {/* <div className=' w-full hidden  order-last md:order-none'>
         {' '}
         <Image
           src={'/Hero2Banner.svg'}
           alt='Hero2Banner'
-          height={330}
-          width={318}
+          height={867}
+          width={867}
         />
-      </div>
+      </div> */}
     </>
   )
 }
@@ -31,23 +31,44 @@ function Hero2() {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 gap-4 w-full md:px-16 p-4 content-center '>
       <ImageComponent />
-      <div className=' my-auto leading-10'>
-        <div className='  '>
-          <h3 className=' text-xl text-bold mb-6'>Why Join Proddly</h3>
-          <ul className='list-disc list-inside mb-6'>
-            <li>
-              Let shoppers find you easily{' '}
-              <span className=' hidden md:inline-block '> and quickly.</span>
+      <div className=' font-semibold my-auto leading-10'>
+        <div className=''>
+          <h3 className=' text-2xl text-bold mb-6'>Why Join Proddly</h3>
+          <ul
+            className='list-disc text-2xl list-inside mb-6 text-cyan-600'
+            style={{ color: '#00baf7' }}
+          >
+            <li className=' my-2 md:my-4'>
+              <span className='text-black text-lg'>
+                Let shoppers find you easily{' '}
+                <span className=' hidden md:inline-block '> and quickly.</span>
+              </span>
             </li>
-            <li>Increase your walk-in and online sales</li>
-            <li>Get online without an e-commerce website</li>
+            <li className=' my-2 md:my-4'>
+              <span className='text-black text-lg'>
+                Increase your walk-in and online sales
+              </span>
+            </li>
+            <li className=' my-2 md:my-4'>
+              <span className='text-black text-lg'>
+                Get online without an e-commerce website
+              </span>
+            </li>
           </ul>
         </div>
         <div>
-          <h3 className='text-xl text-bold mb-6 '>Features</h3>
-          <ul className='list-disc list-inside  '>
-            <li>Proddy 360 Dashboard</li>
-            <li>View What`s in Store </li>
+          <h3 className='text-2xl text-bold mb-6 '>Features</h3>
+          <ul
+            className=' list-disc text-2xl list-inside mb-6 text-cyan-600'
+            style={{ color: '#00baf7' }}
+          >
+            <li className=' my-2 md:my-4 '>
+              {' '}
+              <span className='text-black text-lg'>Proddy 360 Dashboard</span>
+            </li>
+            <li className=' my-2 md:my-4'>
+              <span className='text-black text-lg'>View What`s in Store</span>{' '}
+            </li>
           </ul>
         </div>
       </div>
